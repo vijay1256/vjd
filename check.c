c382366723259ef04bd2a0fe738a460992970977
vijay1256 <vijaykumars.r.t@gmail.com>
2016-07-18 09:38:49 +0530

    Create check.c

---
+++ check.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+
+    printf("Enter a num: ");
+    scanf("%d",num);
+
+    if (num <= 0)
+    {
+	if (num == 0)
+	    printf("You entered 0.");
+	else
+	    printf("You entered a negative number.");
+    }
+    else
+	printf("You entered a positive number.");
+    return 0;
+}
